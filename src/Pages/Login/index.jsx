7950b4e5db9de99898d1ec13523ed4f18de59ac8
unknown <devmarcosav@gmail.com>
2023-05-17 12:51:17 -0300

    feat: criando a pagina de login

---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import { GetStatus } from "../../services/GetStatus";
+import style from "./style.module.css";
+import { useForm } from "react-hook-form";
+
+export const Login = () => {
+ const { register, handleSubmit } = useForm();
+ const navigate = useNavigate();
+ const onSubmit = async (data) => {
+  GetStatus(data);
+  if (localStorage.getItem("apiKey") !== "") {
+   navigate("/home");
+  }
+ };
+
+ return (
+  <div className={style.container}>
+   <div className={style.sidebarBackground} />
+   <div className={style.loginFormContainer}>
+    <h1>Meu Time</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+     <label htmlFor="apiKey">Autenticação</label>
+     <input
+      name="apiKey"
+      placeholder="Api key"
+      {...register("apiKey", { required: true })}
+     />
+     <button className={style.loginFormContainerButton} type="submit">
+      Login
+     </button>
+     <p>
+      Não tem cadastro?
+      <a href="https://dashboard.api-football.com/register">Clique Aqui </a>
+     </p>
+    </form>
+   </div>
+  </div>
+ );
+};
