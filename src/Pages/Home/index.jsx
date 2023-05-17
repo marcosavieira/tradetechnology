cf30cbf05584c82f3a92257cdb49f5b8eb1eb20b
unknown <devmarcosav@gmail.com>
2023-05-17 14:43:37 -0300

    feat: criando logica para exibir ligas e button para limpar

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import style from "./style.module.css";
 import { GetCountries } from "../../services/GetCountries";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AsyncSelect, { useAsync } from "react-select/async";
 
 export const Home = () => {
@@ -9,6 +9,15 @@ export const Home = () => {
  }, []);
 
  const { isLoading, data /* error */ } = useAsync(GetCountries);
+ const [selectedCountryOption, setSelectedCountryOption] = useState("");
+
+ const handleSelectChange = (option) => {
+  setSelectedCountryOption(option.value);
+ };
+ const handleClearSelect = () => {
+  setSelectedCountryOption("");
+ };
+
  return (
   <div className={style.container}>
    <div className={style.navBarCountries}>
@@ -19,8 +28,17 @@ export const Home = () => {
      loadOptions={GetCountries}
      isLoading={isLoading}
      options={data}
+     onChange={handleSelectChange}
      isClearable
     />
+   </div>
+   {selectedCountryOption && (
+    <div className={style.navBarLeagues}>
+     <label>Leagues</label>
+    </div>
+   )}
+   <div className={style.containerButton}>
+    <button onClick={handleClearSelect}>Limpar</button>
    </div>
   </div>
  );
