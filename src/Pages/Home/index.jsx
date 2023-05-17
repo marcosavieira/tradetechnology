fa440571e439f67d4562e80783f9f84104444ee2
unknown <devmarcosav@gmail.com>
2023-05-17 17:48:28 -0300

    feat: listagem de times

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,25 +4,43 @@ import { GetLeagues } from "../../services/GetLeagues";
 import { useEffect, useState } from "react";
 import AsyncSelect, { useAsync } from "react-select/async";
 import { GetSeasons } from "../../services/GetSeasons";
+import { GetTeams } from "../../services/GetTeams";
 
 export const Home = () => {
  const { isLoading, data /* error */ } = useAsync(GetCountries);
  const { isLoadingLeague, dataLeague /* error */ } = useAsync(GetLeagues);
- const { isLoadingSeasons, dataSeasons /* error */ } = useAsync(GetLeagues);
+ const { isLoadingTeams, dataTeams /* error */ } = useAsync(GetTeams);
+ const { isLoadingSeasons, dataSeasons /* error */ } = useAsync(GetSeasons);
  const [selectedCountryOption, setSelectedCountryOption] = useState("");
+ const [selectedLeagueOption, setSelectedLeagueOption] = useState("");
+ /* const [selectedTeamOption, setSelectedTeamOption] = useState(""); */
 
  const handleSelectChange = (option) => {
   setSelectedCountryOption(option.value);
  };
+ const handleSelectChangeLeague = async (option) => {
+  await GetTeams(option.value);
+  return setSelectedLeagueOption(option.value);
+ };
+ /* const handleSelectChangeTeam = async (option) => {
+  await GetPlayers(option.value);
+  return setSelectedTeamOption(option.value);
+ }; */
+
  const handleClearSelect = () => {
   setSelectedCountryOption("");
  };
- useEffect(() => {
-  GetCountries();
+ const fetchData = async () => {
+  await GetCountries();
+  await GetSeasons();
+
   if (selectedCountryOption !== "") {
-   GetLeagues(selectedCountryOption);
+   await GetLeagues(selectedCountryOption);
   }
- }, [selectedCountryOption]);
+ };
+ useEffect(() => {
+  fetchData();
+ }, []);
 
  return (
   <div className={style.container}>
@@ -58,6 +76,20 @@ export const Home = () => {
       loadOptions={GetLeagues}
       isLoading={isLoadingLeague}
       options={dataLeague}
+      onChange={handleSelectChangeLeague}
+      isClearable
+     />
+    </div>
+   )}
+   {selectedLeagueOption && (
+    <div className={style.navBarLeagues}>
+     <label>Times</label>
+     <AsyncSelect
+      cacheOptions
+      defaultOptions
+      loadOptions={GetTeams}
+      isLoading={isLoadingTeams}
+      options={dataTeams}
       isClearable
      />
     </div>
